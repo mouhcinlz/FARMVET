526b1d0f036496c93997b9e63a4c1485bc6ba53d
mouhcinlz <154703960+mouhcinlz@users.noreply.github.com>
2024-05-30 02:07:13 -1200

    Update FarmerProfile.php

--- FarmerProfile.php
+++ FarmerProfile.php
@@ -3,7 +3,6 @@
     include("classes/connect.php");
     include("classes/log-in.php");
     include("classes/users_data.php");
-    include("classess/image.php");
 
 
     // Check if the user is logged in
@@ -48,13 +47,14 @@
         die;
     }
     $error_msg = "";
-    if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+   /* if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 
         
         if(isset($_FILES['image']['name']) && $_FILES['image']['name'] != ""){
 
-            if($_FILES['image']['type'] == "image/jpeg"){
+    
                 $filename = "uploads/" . $_FILES['image']['name'];
                 move_uploaded_file($_FILES['image']['tmp_name'], $filename);
 
@@ -75,14 +75,71 @@
                 $error_msg = "only img type are allowed!";
             }
 
-        }
+        }*/
         
-    }
+ 
 
+    if ($_SERVER['REQUEST_METHOD'] == "POST" ) {
+
+            $firstn = $_POST['firstn'];
+            $lastn = $_POST['lastn'];
+            $mail = $_POST['mail'];
+            $password = $_POST['password'];
+            $loc = $_POST['location'];
+            $date = $_POST['date_birth'];
+
+            $targetDir = "uploads/"; // Directory where images will be stored
+            $targetFile = $targetDir . basename($_FILES["image"]["name"]);
+            $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+        
+            // Check if image file is a actual image or fake image
+            if ($_FILES["image"]["tmp_name"]) {
+                $check = getimagesize($_FILES["image"]["tmp_name"]);
+                if ($check !== false) {
+                    // Image is valid, move it to the target directory
+                    move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile);
+                    $imagePath = $targetFile; // Save the path of the uploaded image
+                } else {
+                    echo "Error: File is not an image.";
+                    exit;
+                }
+            } else {
+                $imagePath = ""; // No image uploaded, set image path to empty
+            }
+
+
+            $userid = $users_d['user_id'];
+            if(!empty($imagePath)){
+                $query = "UPDATE farmer 
+                        SET firstn='$firstn', lastn='$lastn', mail='$mail',
+                        password='$password', image='$imagePath', location='$loc', date_birth='$date' 
+                        WHERE user_id='$userid' LIMIT 1";
+            }
+            else {
+            $query = "UPDATE farmer 
+                        SET firstn='$firstn', lastn='$lastn', mail='$mail',
+                        password='$password', location='$loc', date_birth='$date' 
+                        WHERE user_id='$userid' LIMIT 1";
+            }
+
+            $query2 = "UPDATE users 
+                        SET firstn='$firstn', lastn='$lastn', mail='$mail',
+                        password='$password' WHERE user_id='$userid' LIMIT 1";
+
+            $db = new Database(); // Assuming you have a Database class
+            $db->save($query); // Assuming you have a method to execute SQL queries
+            $db->save($query2);
+            header("Location: farmerProfile.php");
+            exit;
+
+        }
+
+    
     $image = "imgs/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg";
     if(file_exists($users_d['image'])){
         $image = $users_d['image'];
     }
+
 
 
 
@@ -96,10 +153,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Farmer Profile</title>
+    <script src="./js/langcookie.js"></script>
     <link rel="stylesheet" href="css/all.min.css" />
 
-    <link rel="stylesheet" href="css/all_users.css">
-    <link rel="stylesheet" href="css/p.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/profile1.css">
     <link rel="stylesheet" href="css/logout.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -107,6 +165,15 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500&#038;display=swap" rel="stylesheet" />
 </head>
 <body>
+<div class="language-selected" data-i18n="language">
+                        Language
+                    </div>
+                    <ul>
+                        <li><a href="#" class="en" onclick="changeLanguage('en')" data-i18n="en">English</a></li>
+                        <li><a href="#" class="fr" onclick="changeLanguage('fr')" data-i18n="fr">French</a></li>
+                        <li><a href="#" class="ar" onclick="changeLanguage('ar')" data-i18n="ar">Arabic</a></li>
+                    </ul>
+                </div>
     <div class="page ">
         
         <div class="sidebar">
@@ -116,37 +183,37 @@
                     <li>
                     <a class="" href="farmer.php">
                         <i class="fa-solid fa-house fa-fw"></i>
-                        <span>Home</span>
+                        <span data-i18n="home">Home</span>
                     </a>
                     </li>
                     <li>
                     <a class="active" href="farmerProfile.php">
                         <i class="fa-regular fa-user fa-fw"></i>
-                        <span>Profile</span>
+                        <span data-i18n="profile">Profile</span>
                     </a>
                     </li>
                     <li>
                     <a class="" href="all_vet.php">
                         <i class="fa-solid fa-user-doctor fa-fw"></i>
-                        <span>veterinarians</span>
+                        <span data-i18n="veterinarian">veterinarians</span>
                     </a>
                     </li>
                     <li>
                     <a class="" href="messages.html">
                         <i class="fa-solid fa-comments fa-fw"></i>
-                        <span>Messages</span>
+                        <span data-i18n="messages">Messages</span>
                     </a>
                     </li>
                     <li>
                     <a class="" href="store.html">
-                        <i class="fa-solid fa-comments fa-fw"></i>
-                        <span>Store</span>
+                    <i class="fa-solid fa-store"></i>
+                        <span data-i18n="store">Store</span>
                     </a>
                     </li>
                     <li>
                     <a class="" href="contact.html">
                         <i class="fa-solid fa-envelope fa-fw"></i>
-                        <span>Contact Us</span>
+                        <span data-i18n="contact">Contact Us</span>
                     </a>
                     </li>
                 </ul>
@@ -155,7 +222,7 @@
                     <div class="info">
                         <span>@<?php echo $users_d['firstn'] . $users_d['lastn'] ?>
                             <br>
-                            <p>Farmer</p>
+                            <p data-i18n="farmer">Farmer</p>
                         </span>
                     </div>
                 </div>
@@ -168,8 +235,8 @@
             
             <div class="head-box">
                 <div class="title">
-                    <a href="farmer.php" class="active">Your Profile</a>
-                    <a href="all_vet.php" class="">veterinarians</a>
+                    <a href="farmer.php" class="active" data-i18n="UrProfile">Your Profile</a>
+                    <a href="all_vet.php" class="" data-i18n="veterinarian">veterinarians</a>
                     <span class="notification">
                         <i class="fa-regular fa-bell fa-lg"></i>
                     </span>
@@ -185,25 +252,26 @@
                 <div class="card" id="card">
 
                     <div class="info">
+
                     <img src="<?php echo $image; ?>" alt="">
-                    <h2><?php echo $users_d['firstn'] . $users_d['lastn'] ?></h2>
-                    <p>farmer</p>
+                    <h2><?php echo $users_d['firstn'] . " " . $users_d['lastn'] ?></h2>
+                    <p data-i18n="farmer">farmer</p>
                     </div>
 
                     <div class="text">
-                        <span>Your Id:</span>
+                        <span data-i18n="YourID">Your Id:</span>
                         <p><?php echo $users_d['user_id'] ?></p>
                     </div>
                     <div class="text">
-                        <span>E-mail:</span>
+                        <span data-i18n="e_mail">E-mail:</span>
                         <p><?php echo $users_d['mail']?></p>
                     </div>
                     <div class="text">
-                        <span>Date of birth:</span>
-                        <p><?php echo $users_d['date']?></p>
+                        <span data-i18n="dob">Date of birth:</span>
+                        <p><?php echo $users_d['date_birth']?></p>
                     </div>
                     <div class="text">
-                        <span>location:</span>
+                        <span data-i18n="location">location:</span>
                         <p><?php echo $users_d['location'] ?></p>
                     </div>
 
@@ -219,57 +287,55 @@
                     <span id="close-card" class="close-icon">
                         <i class="fa-solid fa-xmark"></i>
                     </span>
-                    <h1>Update Your Info</h1>
+                    <h1 data-i18n="updateInfo">Update Your Info</h1>
+                    
+
                     <div class="lab">
-                        <label>The Old Image:</label>
-                        <img src="<?php  ?>" >
+                        <label data-i18n="newImg">The New Image:</label>
+                        <i class="fa-solid fa-cloud-arrow-up"></i>
+                        <input class="upload" type="file" name="image" id="image">
+
                     </div>
                     <br>
 
                     <div class="lab">
-                        <label>The New Image:</label>
-                        <input type="file" name="image">
+                        <label data-i18n="fname">First Name:</label>
+                        <input type="text" name="firstn" value="<?php echo $users_d['firstn'] ?>">
                     </div>
                     <br>
 
                     <div class="lab">
-                        <label for="name">First Name:</label>
-                        <input type="text" name="firstn" value="<?php  ?>">
+                        <label data-i18n="lname">Last Name:</label>
+                        <input type="text" name="lastn" value="<?php echo $users_d['lastn'] ?>">
                     </div>
                     <br>
 
                     <div class="lab">
-                        <label for="name">Last Name:</label>
-                        <input type="text" name="lastn" value="<?php  ?>">
+                        <label data-i18n="e_mail">Email:</label>
+                        <input type="mail" name="mail" value="<?php echo $users_d['mail'] ?>">
                     </div>
                     <br>
 
                     <div class="lab">
-                        <label for="name">Email:</label>
-                        <input type="mail" name="mail" value="<?php  ?>">
+                        <label data-i18n="password">Password:</label>
+                        <input type="password" name="password" value="<?php echo $users_d['password'] ?>">
                     </div>
                     <br>
 
                     <div class="lab">
-                        <label >Password:</label>
-                        <input type="password" name="password" value="<?php  ?>">
+                        <label data-i18n="dob">date of birth</label>
+                        <input type="date" name="date_birth" value="<?php echo $users_d['date_birth'] ?>">
                     </div>
                     <br>
 
                     <div class="lab">
-                        <label >date of birth</label>
-                        <input type="text" name="date" value="<?php  ?>">
-                    </div>
-                    <br>
-
-                    <div class="lab">
-                        <label for="location">location:</label>
-                        <input type="text" name="location" value="<?php  ?>">
+                        <label for="location" data-i18n="location">location:</label>
+                        <input type="text" name="location" value="<?php echo $users_d['location'] ?>">
                     </div>
                     <br>
 
 
-                    <input class="button" type="submit" name="update" value="update">
+                    <input class="button" type="submit"  value="Update">
                     
                 </div>
 
@@ -284,7 +350,7 @@
                     <input class="p-10" type="search" placeholder="Type A Keyword" />
                     <button class="Btn">
                         <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
-                        <a href="logout.php" class="text" >Logout</a>
+                        <a href="logout.php" class="text" data-i18n="logout">Logout</a>
                     </button>
                 </div>
             </div>
@@ -319,6 +385,33 @@
 
     </script>
     
+    <script>
+        // JavaScript to dynamically switch photos based on screen size
+        window.addEventListener('resize', function () {
+            if (window.innerWidth >= 767) {
+                // Large media screen
+                document.getElementById('img1').style.display = 'block';
+                document.getElementById('img2').style.display = 'none';
+            } else {
+                // Small media screen
+                document.getElementById('img1').style.display = 'none';
+                document.getElementById('img2').style.display = 'block';
+            }
+        });
 
+        // Initial check on page load
+        if (window.innerWidth >= 767) {
+            document.getElementById('img1').style.display = 'block';
+            document.getElementById('img2').style.display = 'none';
+        } else {
+            document.getElementById('img1').style.display = 'none';
+            document.getElementById('img2').style.display = 'block';
+        }
+
+        
+    </script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+    <script src="js/script.js"></script>-->
+    <script src="js/script.js"></script>
 </body>
 </html>
